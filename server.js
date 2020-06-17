201e2f7d3bf75a34d16409ac462635a5f4082bd8
devkhaldi <dev.khaldi@gmail.com>
2020-06-17 16:36:56 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path')
 const app = express()
 require('dotenv').config()
 
-app.get('/', (req, res) => res.json({ message: 'Hello !' }))
-
 // serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
@@ -12,6 +10,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
+
+app.get('/', (req, res) => res.json({ message: 'Hello !' }))
 
 app.use((req, res) => res.status(404).json({ message: 'Not Found!' }))
 
