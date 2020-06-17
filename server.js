5f04ac2f982df0bcff9782f4760e1096f5a695f7
devkhaldi <dev.khaldi@gmail.com>
2020-06-17 15:47:44 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 const app = express()
 require('dotenv').config()
@@ -6,6 +7,14 @@ require('dotenv').config()
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => res.json({ message: 'Hello !' }))
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.use((req, res) => res.status(404).json({ message: 'Not Found!' }))
 
